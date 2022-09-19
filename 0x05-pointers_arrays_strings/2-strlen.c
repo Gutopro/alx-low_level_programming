f3683a3fd1365e6684fe62524ea353ddfbef293f
Nick <nickigber@gmail.com>
2022-09-19 13:10:28 -0700

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,17 @@
 
 /**
 *_strlen - length of string
-*
+*@s: pointer to char
 *@len: varaible for length
+*Return: does not return anything
 */
 
 int _strlen(char *s)
 {
 int len = 0;
-while (s[len] != '\0')
+while (s[len] != '\0';)
+{
 ++len;
+}
 return (len);
 }
