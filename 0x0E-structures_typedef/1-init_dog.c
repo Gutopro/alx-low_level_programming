f09895375c3d80e5aaef4beb073512174ca8fa7c
Nick <nickigber.com>
2022-10-11 13:47:32 -0700

    1-init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,12 @@
 *
 */
 
-init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
 {
-d .name = name;
-d .age = age;
-d .owner = owner;
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
 }
 }
