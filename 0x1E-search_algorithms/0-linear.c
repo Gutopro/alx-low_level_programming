159d10b91ebc4a406fe1a257ba00b8ae4818dd82
nick <nicholasigber@gmail.com>
2023-04-12 23:23:56 +0100

    search Algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,16 +11,14 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-int i;
-
-if (array == NULL)
-return (-1);
-
-for (i = 0; i < (int)size; i++)
-{
-printf("Value checked array[%u] = [%d]\n", i, array[i]);
-if (value == array[i])
-return (i);
+if (array == NULL) {
+return -1;
 }
-return (-1);
+for (size_t i = 0; i < size; i++) {
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value) {
+return i;
+}
+}
+return -1;
 }
